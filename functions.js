2143f14a39dd6125ee4839c6195fc86eb06c4d3f
xybai0103 <xueyin0103@gmail.com>
2023-04-16 17:01:50 -0500

    define class viewAllEmployees

--- functions.js
+++ functions.js
@@ -14,8 +14,28 @@ class viewAllRoles {
     this.db = db;
   }
   render(){
-    db.query('SELECT r.id, r.title, d.name AS department, r.salary FROM role AS r JOIN department AS d ON r.department_id = d.id', function (err, rows) {
-       console.table(rows);
-    });
+    db.query(`SELECT r.id, r.title, d.name AS department, r.salary
+              FROM role AS r 
+              JOIN department AS d 
+              ON r.department_id = d.id`, 
+       function (err, rows) {
+         console.table(rows);
+       });
+  }
+};
+
+class viewAllEmployees {
+  constructor(db) {
+    this.db = db;
+  }
+  render(){
+    db.query(`SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager
+              FROM employee AS e
+              JOIN role AS r ON e.role_id = r.id
+              JOIN department AS d ON r.department_id = d.id
+              LEFT JOIN employee AS m ON e.manager_id = m.id`,   
+       function (err, rows) {
+         console.table(rows);
+       });
   }
 };
