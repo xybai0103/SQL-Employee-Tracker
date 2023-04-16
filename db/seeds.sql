93186c6c0ce5e465782cd7206dbee0c138398c40
xybai0103 <xueyin0103@gmail.com>
2023-04-16 12:04:26 -0500

    create a company_db database containing three tables in schema.sql. Pre-populate database in seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO department (id, name)
+VALUES (1, "Sales"),
+       (2, "Engineering"),
+       (3, "Finance"),
+       (4, "Legal");
+
+INSERT INTO role (id, title, department_id, salary)
+VALUES (1, "Sales Lead", 1, 100000),
+       (2, "Salesperson", 1, 80000),
+       (3, "Lead Engineer", 2, 150000),
+       (4, "Software Engineer", 2, 120000),
+       (5, "Account Manager", 3, 160000),
+       (6, "Accountant", 3, 125000),
+       (7, "Legal Team Lead", 4, 250000),
+       (8, "Lawyer", 4, 190000);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (1, "John", "Doe", 1, null),
+       (2, "Mike", "Chan", 2, 1),
+       (3, "Ashley", "Rodriguez", 3, null),
+       (4, "Kevin", "Tupik", 4, 3),
+       (5, "Kunal", "Singh", 5, null),
+       (6, "Malia", "Brown", 6, 5),
+       (7, "Sarah", "Lourd", 7, null),
+       (8, "Tom", "Allen", 8, 7);
