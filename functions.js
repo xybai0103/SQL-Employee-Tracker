3c28e334e379d1353b6ee9dc05fa39606122dfb0
xybai0103 <xueyin0103@gmail.com>
2023-04-16 15:00:06 -0500

    define class viewAllRoles

--- functions.js
+++ functions.js
@@ -4,9 +4,18 @@ class viewAllDepartments {
   }
   render(){
     db.query('SELECT * FROM department', function (err, rows) {
-        console.table(rows);
-      });
+       console.table(rows);
+    });
   }
 };
 
-
+class viewAllRoles {
+  constructor(db) {
+    this.db = db;
+  }
+  render(){
+    db.query('SELECT r.id, r.title, d.name AS department, r.salary FROM role AS r JOIN department AS d ON r.department_id = d.id', function (err, rows) {
+       console.table(rows);
+    });
+  }
+};
