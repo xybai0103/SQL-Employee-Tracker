d5502f318881ecb3282b56474b3549ee7b590d39
xybai0103 <xueyin0103@gmail.com>
2023-04-17 13:34:23 -0500

    define class updateEmployeeRole

--- functions.js
+++ functions.js
@@ -60,7 +60,7 @@ class addDepartment {
     this.db = db;
   }
   
-  inputAddDepartment(){
+  render(){
     inquirer.prompt([
       {
         type: 'input',
@@ -89,7 +89,7 @@ class addRole {
     this.db = db;
   }
   
-  inputAddRole(){
+  render(){
     db.query('SELECT name FROM department', function (err, results) {
       if (err) { 
         console.error('Error:', err);
@@ -125,7 +125,7 @@ class addRole {
               console.error('Error:', err);
               return;
             }
-        
+            //results is an array containing one object [{id = ...}]
             const departmentId = results[0].id;
 
             db.query(`INSERT INTO role (title, department_id, salary)
@@ -149,96 +149,171 @@ class addEmployee {
     this.db = db;
   }
   
-  inputAddEmployee(){
+  render(){
     db.query(`SELECT r.title, CONCAT(e.first_name, ' ', e.last_name) AS employee
               FROM employee AS e
               JOIN role AS r ON e.role_id = r.id`,
-    function (err, results) {
-      if (err) { 
-        console.error('Error:', err);
-        return;
-      }
+       function (err, results) {
+         if (err) { 
+           console.error('Error:', err);
+           return;
+         }
       
-      // Extract an array of title of roles from the query results
-      const roleNames = results.map((result) => result.title);
-      // Extract an array of employees' names from the query results
-      const employeeNames = results.map((result) => result.employee);
-      employeeNames.push('None');
+         // Extract an array of title of roles from the query results
+         const roleNames = results.map((result) => result.title);
+         // Extract an array of employees' names from the query results
+         const employeeNames = results.map((result) => result.employee);
+         employeeNames.push('None');
 
-      inquirer.prompt([
-        {
-          type: 'input',
-          name: 'firstName',
-          message: "What is the employee's first name?",
-        },
-        {
-          type: 'input',
-          name: 'lastName',
-          message: "What is the employee's last name?",
-        },
-        {
-          type: 'list',
-          name: 'employeeRole',
-          message: "What is the employee's role?",
-          choices: roleNames,
-        },
-        {
-          type: 'list',
-          name: 'employeeManager',
-          message: "Who is the employee's manager?",
-          choices: employeeNames,
-        },
-      ])
+         inquirer.prompt([
+           {
+             type: 'input',
+             name: 'firstName',
+             message: "What is the employee's first name?",
+           },
+           {
+             type: 'input',
+             name: 'lastName',
+             message: "What is the employee's last name?",
+           },
+           {
+             type: 'list',
+             name: 'employeeRole',
+             message: "What is the employee's role?",
+             choices: roleNames,
+           },
+           {
+             type: 'list',
+             name: 'employeeManager',
+             message: "Who is the employee's manager?",
+             choices: employeeNames,
+           },
+         ])
       
-      .then(({firstName, lastName, employeeRole, employeeManager}) => {
-        // Get the id of the role of the added employee that the user input to insert into table employee
-        db.query(`SELECT id FROM role WHERE title = ?`, [employeeRole], function (err, results) {
+         .then(({firstName, lastName, employeeRole, employeeManager}) => {
+           // Get the id of the role of the added employee that the user input to insert into table employee
+           db.query(`SELECT id FROM role WHERE title = ?`, [employeeRole], function (err, results) {
+             if (err) {
+               console.error('Error:', err);
+               return;
+             }
+             //results is an array containing one object [{id = ...}]
+             const roleId = results[0].id;
+
+             // Get the id of the added employee's manager to insert into table employee if the user chose a name
+             if (employeeManager !== 'None') {
+               // split the managerName into an array containing its firstName and lastName
+               const managerName = employeeManager.split(' ');
+               const managerFirstName = managerName[0];
+               const managerLastName = managerName[1];
+               db.query(`SELECT id FROM employee WHERE first_name = ? AND last_name = ?`, [managerFirstName, managerLastName], function (err, results) {
+                 if (err) {
+                   console.error('Error:', err);
+                   return;
+                 }
+                 //results is an array containing one object [{id = ...}]
+                 const managerId = results[0].id;
+
+                 // Insert the added employee into the database
+                 db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id)
+                           VALUES (?, ?, ?, ?);`, [firstName, lastName, roleId, managerId],  
+                   function (err, result) {
+                     if(err){
+                       console.error('Error:', err);
+                     }else{
+                       console.log(`Added ${firstName} ${lastName} to the database`);
+                     }           
+                 });
+               });
+             } 
+            /* If the user chose 'None' for the added employee's manager,
+               no value is provided for manager_id, the column will be 
+               automatically set to NULL in the table.*/
+             else {
+               db.query(`INSERT INTO employee (first_name, last_name, role_id)
+                         VALUES (?, ?, ?, ?);`, [firstName, lastName, roleId],  
+                 function (err, result) {
+                   if(err){
+                     console.error('Error:', err);
+                   }else{
+                     console.log(`Added ${firstName} ${lastName} to the database`);
+                   }           
+               });
+             }
+          });
+       });
+    });
+  }
+};
+
+
+class updateEmployeeRole {
+  constructor(db) {
+    this.db = db;
+  }
+  
+  render(){
+    db.query(`SELECT r.title, CONCAT(e.first_name, ' ', e.last_name) AS employee
+              FROM employee AS e
+              JOIN role AS r ON e.role_id = r.id`,
+      function (err, results) {
+        if (err) { 
+          console.error('Error:', err);
+          return;
+        }
+      
+        // Extract an array of title of roles from the query results
+        const roleNames = results.map((result) => result.title);
+        // Extract an array of employees' names from the query results
+        const employeeNames = results.map((result) => result.employee);
+
+        inquirer.prompt([
+          {
+            type: 'list',
+            name: 'employeeName',
+            message: "Which employee's role do you want to update?",
+            choices: employeeNames,
+          },
+          {
+            type: 'list',
+            name: 'employeeNewRole',
+            message: 'Which role do you want to assign the selected employee?',
+            choices: roleNames,
+          },
+        ])
+      
+        .then(({employeeName, employeeNewRole}) => {
+          // Get the id of the updated role of the employee that the user input
+          db.query(`SELECT id FROM role WHERE title = ?`, [employeeNewRole], function (err, results) {
             if (err) {
               console.error('Error:', err);
               return;
             }
-        
+            //results is an array containing one object [{id = ...}]
             const roleId = results[0].id;
 
-            // Get the id of the added employee's manager to insert into table employee if the user chose a name
-            if (employeeManager !== 'None') {
-              // split the managerName into an array containing its firstName and lastName
-              const managerName = employeeManager.split(' ');
-              const managerFirstName = managerName[0];
-              const managerLastName = managerName[1];
-              db.query(`SELECT id FROM employee WHERE first_name = ? AND last_name = ?`, [managerFirstName, managerLastName], function (err, results) {
-                if (err) {
-                  console.error('Error:', err);
-                  return;
-                }
-                const managerId = results[0].id;
+            const employeeNameSplit = employeeName.split(' ');
+               const employeeFirstName = employeeNameSplit[0];
+               const employeeLastName = employeeNameSplit[1];
+               db.query(`SELECT id FROM employee WHERE first_name = ? AND last_name = ?`, [employeeFirstName, employeeLastName], function (err, results) {
+                 if (err) {
+                   console.error('Error:', err);
+                   return;
+                 }
+                 //results is an array containing one object [{id = ...}]
+                 const employeeId = results[0].id;
 
-                // Insert the added employee into the database
-                db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id)
-                          VALUES (?, ?, ?, ?);`, [firstName, lastName, roleId, managerId],  
-                  function (err, result) {
-                    if(err){
-                      console.error('Error:', err);
-                    }else{
-                      console.log(`Added ${firstName} ${lastName} to the database`);
-                    }           
-                });
-              });
-            } 
-            /* If the user chose 'None' for the added employee's manager,
-               no value is provided for manager_id, the column will be 
-               automatically set to NULL in the table.*/
-            else {
-              db.query(`INSERT INTO employee (first_name, last_name, role_id)
-                        VALUES (?, ?, ?, ?);`, [firstName, lastName, roleId],  
-                function (err, result) {
-                  if(err){
-                    console.error('Error:', err);
-                  }else{
-                    console.log(`Added ${firstName} ${lastName} to the database`);
-                  }           
-              });
-            }
+                 db.query(`UPDATE employee
+                           SET role_id = ?
+                           WHERE id = ?;`, [roleId, employeeId],  
+                   function (err, result) {
+                     if(err){
+                       console.error('Error:', err);
+                     }else{
+                       console.log(`Updated employee's role`);
+                     }           
+                 });
+               });
           });
        });
     });
